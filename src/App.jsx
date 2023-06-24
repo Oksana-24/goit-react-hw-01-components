16641910b615e948e1e0931d8db38a8005f5f925
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-06-24 23:21:58 +0200

    add new components to App

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Profile } from "./components/Profile/Profile";
+import { Statistics } from "components/Statistics/Statistics";
+import { FriendList } from "components/FriendList/FriendList";
+import { TransactionHistory } from "components/TransactionHistory/TransactionHistory";
+
+
+import user from "./components/Profile/user";
+import data from "./components/Statistics/data";
+import friends from "./components/FriendList/friends";
+import transactions from "./components/TransactionHistory/transactions"
+
+
+export const App = () => {
+  return (
+    <>
+      <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats} />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions}/>
+    </>
+    );
+};

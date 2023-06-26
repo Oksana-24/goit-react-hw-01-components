008070276f2a7c6a2a76f07f1f72f3afd5329b90
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-06-26 16:53:16 +0200

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,15 +1,19 @@
-
+import css from "./FriendList.module.css";
+import clsx from "clsx";
 
 export const FriendList = ({friends}) => {
 return (
-    <ul> 
+    <ul className={css.friendList}> 
     {
         friends.map(({avatar, name, isOnline, id}) => {
             return (
-                <li key={id}>
-                    <span></span>
-                    <img src={avatar} alt={name} />
-                    <p>{ name}</p>
+                <li key={id} className={css.friendItem}>
+                    <span className={clsx(
+                        css.circle,
+                        isOnline ? css.isOnline:css.isOffline
+                    )}></span>
+                    <img className={css.friendPhoto } width="120px" src={avatar} alt={name} />
+                    <p className={css.friendName}>{ name}</p>
             </li>
         )
     })

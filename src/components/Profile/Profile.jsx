5ade8510d9b156307f0b54255b23351d9a4c447e
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-06-26 17:35:42 +0200

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import { Avatar, DescriptionAvatar, ListStats, ProfileSection } from "./Profile.styled"
+import PropTypes from 'prop-types'
 
 export const Profile = ({ username, tag, location, avatar, stats: { followers, views, likes } }) => {
 return (
@@ -30,4 +31,16 @@ return (
     </ListStats>
 </ProfileSection>
     )
+}
+
+Profile.propTypes = {
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.exact({
+        followers: PropTypes.number.isRequired,
+        views: PropTypes.number.isRequired,
+        likes: PropTypes.number.isRequired,
+    }).isRequired,
 }

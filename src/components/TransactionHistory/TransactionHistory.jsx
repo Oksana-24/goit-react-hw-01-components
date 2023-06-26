d5b600d5674bc544607de79c288f1474d90eb22e
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-06-26 17:35:37 +0200

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,4 +1,5 @@
-import css from "./TransactionHistory.module.css"
+import css from "./TransactionHistory.module.css";
+import PropTypes from 'prop-types'
 
 export const TransactionHistory = ({ items }) => {
   return (
@@ -38,4 +39,15 @@ export const TransactionHistory = ({ items }) => {
   </tbody> */}
 </table>
   )
+}
+
+TransactionHistory.propTypes = {
+items: PropTypes.arrayOf(
+  PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    amount: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired
+  })
+)
 }

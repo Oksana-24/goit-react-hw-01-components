e47fb64b4a4d38a8ecb3232bd2f247a615e0ef73
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-06-27 13:29:52 +0200

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,37 +1,44 @@
-import { Avatar, DescriptionAvatar, ListStats, ProfileSection } from "./Profile.styled"
-import PropTypes from 'prop-types'
+import {
+    Avatar,
+    DescriptionAvatar,
+    ListStats,
+    ProfileSection,
+} from './Profile.styled';
+import PropTypes from 'prop-types';
 
-export const Profile = ({ username, tag, location, avatar, stats: { followers, views, likes } }) => {
-return (
-<ProfileSection>
-    <DescriptionAvatar>
-    <Avatar
-        src={avatar}
-        alt="User avatar"
-        className="avatar"
-    />
-    <p className="name">{username}</p>
-    <p className="tag">@{tag}</p>
-    <p className="location">{location}</p>
-    </DescriptionAvatar>
+export const Profile = ({
+    username,
+    tag,
+    location,
+    avatar,
+    stats: { followers, views, likes },
+}) => {
+    return (
+        <ProfileSection>
+            <DescriptionAvatar>
+                <Avatar src={avatar} alt="User avatar" className="avatar" />
+                <p className="name">{username}</p>
+                <p className="tag">@{tag}</p>
+                <p className="location">{location}</p>
+            </DescriptionAvatar>
 
-    <ListStats>
-    <li>
-        <span className="label">Followers</span>
-        <span className="quantity">{followers}</span>
-    </li>
-    <li>
-        <span className="label">Views</span>
-        <span className="quantity">{views}</span>
-    </li>
-    <li>
-        <span className="label">Likes</span>
-        <span className="quantity">{likes}</span>
-    </li>
-    </ListStats>
-</ProfileSection>
-    )
-}
+            <ListStats>
+                <li>
+                    <span className="label">Followers</span>
+                    <span className="quantity">{followers}</span>
+                </li>
+                <li>
+                    <span className="label">Views</span>
+                    <span className="quantity">{views}</span>
+                </li>
+                <li>
+                    <span className="label">Likes</span>
+                    <span className="quantity">{likes}</span>
+                </li>
+            </ListStats>
+        </ProfileSection>
+    );
+};
 
 Profile.propTypes = {
     username: PropTypes.string.isRequired,
@@ -43,4 +50,4 @@ Profile.propTypes = {
         views: PropTypes.number.isRequired,
         likes: PropTypes.number.isRequired,
     }).isRequired,
-}
+};

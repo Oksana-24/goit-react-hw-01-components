a8612a4f4f7bd8423c8757d0cd16da0cd81ffe32
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-06-27 13:29:57 +0200

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,34 +1,39 @@
-import css from "./FriendList.module.css";
-import clsx from "clsx";
+import css from './FriendList.module.css';
+import clsx from 'clsx';
 import PropTypes from 'prop-types';
 
-export const FriendList = ({friends}) => {
-return (
-    <ul className={css.friendList}> 
-    {
-        friends.map(({avatar, name, isOnline, id}) => {
-            return (
-                <li key={id} className={css.friendItem}>
-                    <span className={clsx(
-                        css.circle,
-                        isOnline ? css.isOnline:css.isOffline
-                    )}></span>
-                    <img className={css.friendPhoto } width="120px" src={avatar} alt={name} />
-                    <p className={css.friendName}>{ name}</p>
-            </li>
-        )
-    })
-    }
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => {
+        return (
+          <li key={id} className={css.friendItem}>
+            <span
+              className={clsx(
+                css.circle,
+                isOnline ? css.isOnline : css.isOffline
+              )}
+            ></span>
+            <img
+              className={css.friendPhoto}
+              width="120px"
+              src={avatar}
+              alt={name}
+            />
+            <p className={css.friendName}>{name}</p>
+          </li>
+        );
+      })}
     </ul>
-)
-}
+  );
+};
 FriendList.propTypes = {
-friends: PropTypes.arrayOf(
+  friends: PropTypes.arrayOf(
     PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        avatar: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        isOnline: PropTypes.bool.isRequired
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
     })
-).isRequired
-}
+  ).isRequired,
+};
